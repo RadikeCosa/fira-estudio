c91fde410af90721e43c80e7ea4d0dddb8bb63db
Radike Cosa <ramirocosa@gmail.com>
2026-01-18 16:51:28 -0300

    fix props featuredProductSection

--- components/home/FeaturedProductsSection.tsx
+++ components/home/FeaturedProductsSection.tsx
@@ -7,6 +7,7 @@ import { ArrowRight } from "lucide-react";
 import { cn } from "@/lib/utils";
 import type { ProductoCompleto } from "@/lib/types";
 
+interface FeaturedProductsSectionProps {
   productos: ProductoCompleto[];
 }
 
