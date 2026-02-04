01361be94057eae7bd3e3c434e57dcba4dfd3fa5
Radike Cosa <ramirocosa@gmail.com>
2026-02-04 15:06:41 -0300

    fix

--- lib/mercadopago/client.ts
+++ lib/mercadopago/client.ts
@@ -5,7 +5,7 @@ const client = new MercadoPagoConfig({
   accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN!,
   options: {
     timeout: 5000,
-    integrator_id: process.env.MERCADOPAGO_INTEGRATOR_ID,
+    integratorId: process.env.MERCADOPAGO_INTEGRATOR_ID,
   },
 });
 
